aa10c6a2c08dd913bd50ac8ed47088663fa67bba
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-27 09:22:42 -0400

    findHDF5 template
    findHDF5: optimize link checks

--- out3f_mpi_h5.f90
+++ out3f_mpi_h5.f90
@@ -45,16 +45,18 @@ integer(hsize_t) :: cdims(drank) = [1,1,1,1] ! chunks data dimensions
 !---------------------------------------------------!
 
 !------------------ miscellaneous ------------------!
-character(len=3) :: c                                     ! dataset name for specific rank
+character(len=3) :: c
+!! dataset name for specific rank
 character(len=10) :: dataset_name
 character(mpi_max_processor_name) hostname
 
 integer :: mtotal(nDim)
-integer(hsize_t) :: dimsf(drank) ! data dataset dimensions
+integer(hsize_t) :: dimsf(drank)
+!! data dataset dimensions
 integer :: i,j,k,l,m,info,idd
-  real*8 :: ngrids_out
-character*20 fname
-!---------------------------------------------------!
+real(real64) :: ngrids_out
+character(:), allocatable :: fname
+
 
 ! initialize HDF5 fortran interface
 call h5open_f(ierr)
@@ -68,148 +70,144 @@ dimsf(4) = mz
 
 info = mpi_info_null
 
-fname = 'fort.q' // &
-char(ichar('0') + mod(iframe/1000,10)) // &
-char(ichar('0') + mod(iframe/100,10)) // &
-char(ichar('0') + mod(iframe/10,10)) // &
-char(ichar('0') + mod(iframe,10)) // &
-'.h5'
-
+write(fname, "(A6,I4,A3)") 'fort.q', mod(iframe,10), '.h5'
 
 ! have id 0 creates hdf5 data layout and write all attributes
-if (id == 0) then
-
-  ! create datatype for the attribute
-  ! Copy existing datatype
-  ! H5T_NATIVE_CHARACTER - copy this datatype
-  ! atype_id - copy datatype to this variable
-  call h5tcopy_f(h5t_native_double,atype_id,ierr)
-
-  ! Current version sets chunks size as whole dataset of q
-  cdims(1) = 1 !dimsf(1)
-  cdims(2:4) = dimsf(2:4)
-
-  ! create scalar dataspace for the attribute
-  call h5screate_simple_f(arank,adims,aspace_id, ierr)
-
-  ! create the hdf5 file
-  ! filename: name of current file
-  ! h5f_acc_trunc_f: rewrite if file exists
-  ! file_id: identifier for file
-  call h5fcreate_f(fname, h5f_acc_trunc_f, file_id, ierr)
-
-  ! create the dataspace for the dataset
-  ! rank: rank of datasets
-  ! dimsf: size of every dimension
-  ! dataspace_id: identifier of dataspace
-  call h5screate_simple_f(drank, dimsf, dataspace_id, ierr)
-
-  ! create properties variable for the data
-  ! h5p_dataset_create_f: property to create a data dataset
-  ! dcpl: variable for data dataset properties
-  call h5pcreate_f(h5p_dataset_create_f, dcpl, ierr)
-
-  ! attribute the chunk size
-  ! dcpl: link this property variable with chunk size
-  ! 4: dimension of q
-  ! cdims: dimensions of chunk in every direction
-  call h5pset_chunk_f(dcpl, 4, cdims, ierr)
-
-  ! attribute the compression type (GZIP compression)
-  ! dcpl: link this property variable with filter
-  ! 6: compression rank
-  call h5pset_deflate_f(dcpl, 6, ierr)
-
-  ! Uncomment below and comment previous line to use SZIP compression
-
-  !szip_options_mask = H5_SZIP_NN_OM_F
-  !szip_pixels_per_block = 16
-  !CALL H5Pset_szip_f(dcpl, szip_options_mask, szip_pixels_per_block, ierr)
-
-  ! attribute time of allocation of space for data in datasets
-  ! h5d_alloc_time_early_f - allocate all space when the dataset is created
-  call h5pset_alloc_time_f(dcpl, h5d_alloc_time_early_f, ierr)
-
-  ! create name for every dataset
-  create: do i=1,np
-
-    write(c,"(i0)") i
-    dataset_name = "Pid" // trim(c)
-
-    ! create dataset for this processor (based on id)
-    ! file_id: name of file where to create dataset
-    ! dataset_name: name of the dataset
-    ! h5t_native_integer: type of data in dataset
-    ! dataspace_id: dataspace used for dataset
-    ! dataset_id: identifier for dataset
-    ! dcpl_id: dataset creation property list
-    call h5dcreate_f(file_id, dataset_name, h5t_native_double, &
-                        dataspace_id, dataset_id, ierr, dcpl_id=dcpl)
-
-    if (i == 1) then
-      ! Attributes list is created only for MASTER
-
-      attr_datacur(1) = ngrids_out
-      attr_datacur(2) = nDim
-      attr_datacur(3) = t
-      attr_datacur(4) = meqn
-      attr_datacur(5) = 1.
-      attr_datacur(6) = mtotal(1) ! Full mx (all processors)
-      attr_datacur(7) = mtotal(2) ! Full my
-      attr_datacur(8) = mtotal(3) ! Full mz
-      attr_datacur(9) = 0.
-      attr_datacur(10) = xlower ! lower boundary x
-      attr_datacur(11) = ylower ! lower boundary y
-      attr_datacur(12) = zlower ! lower boundary z
-      attr_datacur(13) = 0
-      attr_datacur(14) = xlower+mtotal(1)*dx ! upper boundary x
-      attr_datacur(15) = ylower+mtotal(2)*dy ! upper boundary y
-      attr_datacur(16) = zlower+mtotal(3)*dz ! upper boundary z
-      attr_datacur(17) = 0.
-      attr_datacur(18) = dx
-      attr_datacur(19) = dy
-      attr_datacur(20) = dz
-      attr_datacur(21) = 0.
-      ! Next variable are not included for HDF4 version but needed for slicing in HDF5
-      attr_datacur(22) = lx
-      attr_datacur(23) = ly
-      attr_datacur(24) = lz
-      attr_datacur(25) = iframe ! Control variable
-
-      call h5acreate_f(dataset_id,"Parameters",atype_id,aspace_id,attr_id, ierr)
-
-      data_dims(1) = 25
-      call h5awrite_f(attr_id, atype_id, attr_datacur, data_dims, ierr)
-
-      ! close attribute
-      call h5aclose_f(attr_id, ierr)
-
-      ! close access to the dataspace for attribute
-      call h5sclose_f(aspace_id, ierr)
-
-      call h5tclose_f(atype_id, ierr)
-
-    end if
-
-    ! close dataset
-    call h5dclose_f(dataset_id, ierr)
-
-  enddo create
-
-
-  ! close the dataspace
-  call h5sclose_f(dataspace_id, ierr)
-
-  ! close the properties variable
-  call h5pclose_f(dcpl, ierr)
-
-  ! close the file
-  call h5fclose_f(file_id, ierr)
-end if
+if (id == 0) call creator()
 
 ! mpi barrier to make sure everything is synched
 call mpi_barrier(mpi_comm_3d, ierr)
 
+call writer()
+
+
+contains
+
+
+subroutine creator()
+
+! create datatype for the attribute
+! Copy existing datatype
+! H5T_NATIVE_CHARACTER - copy this datatype
+! atype_id - copy datatype to this variable
+call h5tcopy_f(h5t_native_double,atype_id,ierr)
+
+! Current version sets chunks size as whole dataset of q
+cdims(1) = 1 !dimsf(1)
+cdims(2:4) = dimsf(2:4)
+
+! create scalar dataspace for the attribute
+call h5screate_simple_f(arank,adims,aspace_id, ierr)
+
+! create the hdf5 file
+call h5fcreate_f(fname, h5f_acc_trunc_f, file_id, ierr)
+
+! create the dataspace for the dataset
+! dimsf: size of every dimension
+call h5screate_simple_f(drank, dimsf, dataspace_id, ierr)
+
+! create properties variable for the data
+call h5pcreate_f(h5p_dataset_create_f, dcpl, ierr)
+
+! attribute the chunk size
+! dcpl: link this property variable with chunk size
+! 4: dimension of q
+! cdims: dimensions of chunk in every direction
+call h5pset_chunk_f(dcpl, 4, cdims, ierr)
+
+! attribute the compression type (GZIP compression)
+call h5pset_deflate_f(dcpl, 6, ierr)
+
+! Uncomment below and comment previous line to use SZIP compression
+
+!szip_options_mask = H5_SZIP_NN_OM_F
+!szip_pixels_per_block = 16
+!CALL H5Pset_szip_f(dcpl, szip_options_mask, szip_pixels_per_block, ierr)
+
+! attribute time of allocation of space for data in datasets
+! h5d_alloc_time_early_f - allocate all space when the dataset is created
+call h5pset_alloc_time_f(dcpl, h5d_alloc_time_early_f, ierr)
+
+! create name for every dataset
+create: do i=1,np
+
+  write(c,"(i0)") i
+  dataset_name = "Pid" // trim(c)
+
+  ! create dataset for this processor (based on id)
+  ! file_id: name of file where to create dataset
+  ! dataset_name: name of the dataset
+  ! h5t_native_integer: type of data in dataset
+  ! dataspace_id: dataspace used for dataset
+  ! dataset_id: identifier for dataset
+  ! dcpl_id: dataset creation property list
+  call h5dcreate_f(file_id, dataset_name, h5t_native_double, &
+                      dataspace_id, dataset_id, ierr, dcpl_id=dcpl)
+
+  if (i == 1) then
+    ! Attributes list is created only for MASTER
+
+    attr_datacur(1) = ngrids_out
+    attr_datacur(2) = nDim
+    attr_datacur(3) = t
+    attr_datacur(4) = meqn
+    attr_datacur(5) = 1.
+    attr_datacur(6) = mtotal(1) ! Full mx (all processors)
+    attr_datacur(7) = mtotal(2) ! Full my
+    attr_datacur(8) = mtotal(3) ! Full mz
+    attr_datacur(9) = 0.
+    attr_datacur(10) = xlower ! lower boundary x
+    attr_datacur(11) = ylower ! lower boundary y
+    attr_datacur(12) = zlower ! lower boundary z
+    attr_datacur(13) = 0
+    attr_datacur(14) = xlower+mtotal(1)*dx ! upper boundary x
+    attr_datacur(15) = ylower+mtotal(2)*dy ! upper boundary y
+    attr_datacur(16) = zlower+mtotal(3)*dz ! upper boundary z
+    attr_datacur(17) = 0.
+    attr_datacur(18) = dx
+    attr_datacur(19) = dy
+    attr_datacur(20) = dz
+    attr_datacur(21) = 0.
+    ! Next variable are not included for HDF4 version but needed for slicing in HDF5
+    attr_datacur(22) = lx
+    attr_datacur(23) = ly
+    attr_datacur(24) = lz
+    attr_datacur(25) = iframe ! Control variable
+
+    call h5acreate_f(dataset_id,"Parameters",atype_id,aspace_id,attr_id, ierr)
+
+    data_dims(1) = 25
+    call h5awrite_f(attr_id, atype_id, attr_datacur, data_dims, ierr)
+
+    ! close attribute
+    call h5aclose_f(attr_id, ierr)
+
+    ! close access to the dataspace for attribute
+    call h5sclose_f(aspace_id, ierr)
+
+    call h5tclose_f(atype_id, ierr)
+
+  end if
+
+  ! close dataset
+  call h5dclose_f(dataset_id, ierr)
+
+enddo create
+
+
+! close the dataspace
+call h5sclose_f(dataspace_id, ierr)
+
+! close the properties variable
+call h5pclose_f(dcpl, ierr)
+
+! close the file
+call h5fclose_f(file_id, ierr)
+
+end subroutine creator
+
+
+subroutine writer()
 ! setup file access property variable with parallel i/o access
 ! plist_id: property variable
 ! comm - mpi communicator for mpi/io
@@ -273,6 +271,6 @@ call h5fclose_f(file_id, ierr)
 ! close fortran interface
 call h5close_f(ierr)
 
-end subroutine
+end subroutine writer
 
 end module original
