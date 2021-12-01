90c3c7b048007f890885e1bfebc9d6cbea8724c5
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-01 17:18:00 -0500

    remove vendored pathlib

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,6 +1,6 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   add_compile_options(
-    $<$<CONFIG:Debug>:-Werror=array-bounds>
+    "$<$<CONFIG:Debug,RelWithDebInfo>:-Werror=array-bounds,-fcheck=all>"
     $<$<CONFIG:Release>:-fno-backtrace>
   )
 endif()
