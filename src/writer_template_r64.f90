d0474636339bfea75727380a78bcd43c928b5742
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-09 23:26:25 -0500

    lint: explict args

--- src/writer_template_r64.f90
+++ src/writer_template_r64.f90
@@ -1,8 +1,8 @@
 integer :: ierr
 integer(HID_T) :: filespace, memspace, dset_id, plist_id
 
-call hdf_create(self, dname, H5T_NATIVE_DOUBLE, shape(A, HSIZE_T), dims_file, &
-  filespace, memspace, dset_id, plist_id=plist_id)
+call hdf_create(self, dname, dtype=H5T_NATIVE_DOUBLE, dims=shape(A, HSIZE_T), dims_file=dims_file, &
+  filespace=filespace, memspace=memspace, dset_id=dset_id, plist_id=plist_id)
 
 call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, A, dims_file, ierr, &
   file_space_id=filespace, &
