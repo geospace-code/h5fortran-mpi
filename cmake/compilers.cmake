80296baa76b6326bb8dbf68ed940d5f08b44d278
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-10 10:00:36 -0500

    better debug options

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,6 +1,18 @@
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  add_compile_options(
-    "$<$<CONFIG:Debug,RelWithDebInfo>:-Werror=array-bounds,-fcheck=all>"
-    $<$<CONFIG:Release>:-fno-backtrace>
-  )
+
+add_compile_options(
+"$<$<COMPILE_LANGUAGE:Fortran>:-mtune=native;-Wall;-fimplicit-none>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-Wextra;-fcheck=all;-Werror=array-bounds>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>"
+)
+#"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:RelWithDebInfo>>:-Wno-maybe-uninitialized>"
+
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+
+add_compile_options(
+$<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>
+"$<$<COMPILE_LANGUAGE:Fortran>:-warn;-heap-arrays>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-traceback;-check;-debug>"
+)
+
 endif()
