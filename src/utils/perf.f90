2902c6fea56e626247baf8759183b6c538166bf4
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-17 16:37:10 -0500

    write stats to HDF5 file

--- src/utils/perf.f90
+++ src/utils/perf.f90
@@ -3,6 +3,9 @@ module perf
 use, intrinsic :: iso_fortran_env, only : int64, stderr=>error_unit, stdout=>output_unit
 use median_mod, only : median
 
+use h5mpi, only : hdf5_file
+use hdf5, only : HSIZE_T
+
 implicit none (type, external)
 
 contains
@@ -22,22 +25,24 @@ sysclock2ms = real(t) * r
 end function sysclock2ms
 
 
-subroutine print_timing(bits, dims, t_elapsed, file_bytes)
+subroutine print_timing(bits, dims, t_elapsed, file_bytes, outfn)
 !! print summary of results
 integer, intent(in) :: bits, dims(:)
 integer(int64), intent(in) :: t_elapsed(:)
 real, intent(in) :: file_bytes
+character(*), intent(in), optional :: outfn
 
 integer :: bytes,N
 real :: data_MB, file_MB, mean_MBsec, var_MBsec, std_MBsec, median_MBsec
 real, dimension(size(t_elapsed)) :: t_ms, data_MBsec
 
+type(hdf5_file) :: f
+
 N = size(t_ms)
 
 t_ms = sysclock2ms(t_elapsed)
 
-print '(a15,2x,a15,1x,a15,2x,a9,2x,a9)', &
-  'mean (MB/sec)', 'median (MB/sec)', 'stdev (MB/sec)', 'data (MB)','file (MB)'
+print '(a15,1x,a15,2x,a9,2x,a9)', 'median (MB/sec)', 'stdev (MB/sec)', 'data (MB)','file (MB)'
 
 bytes = bits/8
 data_MB = bytes * real(product(dims) + product(dims(:2))) / 1024 / 1024
@@ -50,13 +55,25 @@ std_MBsec = sqrt(var_MBsec)
 median_MBsec = median(data_MBsec)
 
 file_MB = file_bytes / 1024 / 1024
-print '(f15.1,2x,f15.1,1x,f15.3,1x,f9.1,1x,f9.1)', mean_MBsec, median_MBsec, std_MBsec, data_MB, file_MB
-
-print "(A, 100f7.1)", "TRACE: time (ms)", t_ms
-print "(A, 100f7.1)", "TRACE: MB/sec", data_MBsec
+print '(f15.1,1x,f15.3,1x,f9.1,1x,f9.1)', median_MBsec, std_MBsec, data_MB, file_MB
 
 if(mean_MBsec < 10) write(stderr,'(a)') "WARNING: write speed seems unusually slow."
 if(data_MB < 1) write(stderr, '(a)') "WARNING: benchmark loses accuracy with small files."
+
+if(.not.present(outfn)) return
+
+call f%open(outfn, action="w", mpi=.false.)
+
+call f%write("t_ms", t_ms, shape(t_ms, HSIZE_T))
+call f%write("MBsec", data_MBsec, shape(data_MBsec, HSIZE_T))
+call f%write("mean_MBsec", mean_MBsec)
+call f%write("var_MBsec", var_MBsec)
+call f%write("std_MBsec", std_MBsec)
+call f%write("median_MBsec", median_MBsec)
+call f%write("data_MB", data_MB)
+call f%write("file_MB", file_MB)
+
+call f%close()
 
 end subroutine
 
