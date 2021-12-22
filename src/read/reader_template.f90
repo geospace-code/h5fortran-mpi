785fbba1cd81e73ff52454d3c8cb7c68c2788c3f
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-22 17:33:00 -0500

    read: use hdf_wrapup to avoid leaks

--- src/read/reader_template.f90
+++ src/read/reader_template.f90
@@ -1,32 +1,32 @@
 integer(HSIZE_T) :: dims(rank(value))
-integer(HID_T) :: ds_id, space_id, mem_space_id, native_dtype, xfer_id
+integer(HID_T) :: dset_id, file_space_id, mem_space_id, native_dtype, xfer_id
 integer :: ier
 
 mem_space_id = H5S_ALL_F
 
 dims = shape(value, HSIZE_T)
 
-call h5open_read(self, dname, dims, space_id, ds_id, xfer_id)
+call h5open_read(self, dname, dims, file_space_id, dset_id, xfer_id)
 
-native_dtype = get_native_dtype(ds_id, dname, self%filename)
+native_dtype = get_native_dtype(dset_id, dname, self%filename)
 
 !> casting is handled by HDF5 library internally
 !! select case doesn't allow H5T_*
 if(native_dtype == H5T_NATIVE_DOUBLE .or. native_dtype == H5T_NATIVE_REAL) then
   select type(value)
   type is (real(real64))
-    call h5dread_f(ds_id, H5T_NATIVE_DOUBLE, value, dims, ier, mem_space_id, space_id, xfer_id)
+    call h5dread_f(dset_id, H5T_NATIVE_DOUBLE, value, dims, ier, mem_space_id, file_space_id, xfer_id)
   type is (real(real32))
-    call h5dread_f(ds_id, H5T_NATIVE_REAL, value, dims, ier, mem_space_id, space_id, xfer_id)
+    call h5dread_f(dset_id, H5T_NATIVE_REAL, value, dims, ier, mem_space_id, file_space_id, xfer_id)
   class default
     error stop 'h5fortran:read: real disk dataset ' // dname // ' needs real memory variable'
   end select
 elseif(native_dtype == H5T_NATIVE_INTEGER .or. native_dtype == H5T_STD_I64LE) then
   select type(value)
   type is (integer(int32))
-    call h5dread_f(ds_id, H5T_NATIVE_INTEGER, value, dims, ier, mem_space_id, space_id, xfer_id)
+    call h5dread_f(dset_id, H5T_NATIVE_INTEGER, value, dims, ier, mem_space_id, file_space_id, xfer_id)
   type is (integer(int64))
-    call h5dread_f(ds_id, H5T_STD_I64LE, value, dims, ier, mem_space_id, space_id, xfer_id)
+    call h5dread_f(dset_id, H5T_STD_I64LE, value, dims, ier, mem_space_id, file_space_id, xfer_id)
   class default
     error stop 'h5fortran:read: integer disk dataset ' // dname // ' needs integer memory variable'
   end select
@@ -35,5 +35,4 @@ else
 end if
 if(ier/=0) error stop 'h5fortran:reader: reading ' // dname // ' from ' // self%filename
 
-call h5dclose_f(ds_id, ier)
-if (ier/=0) error stop 'h5fortran:reader: error closing dataset ' // dname // ' in ' // self%filename
+call hdf_wrapup(file_space_id, mem_space_id, dset_id, xfer_id)
--- src/write/writer_template.f90
+++ src/write/writer_template.f90
@@ -1,4 +1,4 @@
-integer(HID_T) :: filespace, memspace, dset_id, xfer_id, dtype
+integer(HID_T) :: file_space_id, mem_space_id, dset_id, xfer_id, dtype
 integer(HSIZE_T) :: dims(rank(value))
 integer :: ier
 
@@ -18,20 +18,20 @@ class default
 end select
 
 call hdf_create(self, dname, dtype, dims, dims_file=dims_file, &
-  filespace=filespace, memspace=memspace, dset_id=dset_id, xfer_id=xfer_id)
+  filespace=file_space_id, memspace=mem_space_id, dset_id=dset_id, xfer_id=xfer_id)
 
 select type (value)
 type is (real(real32))
-  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=filespace, mem_space_id=memspace, xfer_prp=xfer_id)
+  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=file_space_id, mem_space_id=mem_space_id, xfer_prp=xfer_id)
 type is (real(real64))
-  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=filespace, mem_space_id=memspace, xfer_prp=xfer_id)
+  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=file_space_id, mem_space_id=mem_space_id, xfer_prp=xfer_id)
 type is (integer(int32))
-  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=filespace, mem_space_id=memspace, xfer_prp=xfer_id)
+  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=file_space_id, mem_space_id=mem_space_id, xfer_prp=xfer_id)
 type is (integer(int64))
-  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=filespace, mem_space_id=memspace, xfer_prp=xfer_id)
+  call h5dwrite_f(dset_id, dtype, value, dims_file, ier, file_space_id=file_space_id, mem_space_id=mem_space_id, xfer_prp=xfer_id)
 class default
   error stop "unknown variable type for " // dname
 end select
 if (ier/=0) error stop "h5dwrite: " // dname // " " // self%filename
 
-call hdf_wrapup(filespace, memspace, dset_id, xfer_id)
+call hdf_wrapup(file_space_id, mem_space_id, dset_id, xfer_id)
