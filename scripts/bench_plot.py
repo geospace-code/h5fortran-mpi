aed662a52eceb5d56907e11a6990caf4242dd416
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-23 23:25:45 -0500

    time plots from accurate data

--- scripts/bench_plot.py
+++ scripts/bench_plot.py
@@ -4,13 +4,14 @@ import shutil
 import subprocess
 import platform
 
+import numpy as np
 import pandas as pd
 from matplotlib.figure import Figure
 
 from utils import cli
 
 
-def plot_datarate(dr: pd.DataFrame):
+def plot_datarate(dr: pd.DataFrame) -> tuple:
     """plot benchmarks"""
 
     fig = Figure()
@@ -26,7 +27,7 @@ def plot_datarate(dr: pd.DataFrame):
     return fig, ax
 
 
-def plot_time(t: pd.DataFrame):
+def plot_time(t: pd.DataFrame) -> tuple:
     """plot benchmarks"""
 
     fig = Figure()
@@ -66,32 +67,38 @@ def title_meta(lx: tuple[int, int, int], bin_dir: Path) -> str:
     return ttxt
 
 
-def get_bench(lx: tuple[int, int, int], data_dir: Path) -> tuple[pd.DataFrame, pd.DataFrame]:
+def get_bench(lx: tuple[int, int, int], data_dir: Path) -> dict[str, pd.DataFrame]:
     """get writing benchmark HDF5 files to pandas dataframe"""
 
     comp_lvls = [0, 1, 3, 5, 7, 9]
     tests = ["serial", "mpi_root", "mpi_hdf5"]
 
-    write_datarate = pd.DataFrame(index=comp_lvls, columns=tests)
-    read_datarate = pd.DataFrame(index=comp_lvls, columns=tests)
+    data = {
+        "write_dr": pd.DataFrame(index=comp_lvls, columns=tests),
+        "read_dr": pd.DataFrame(index=comp_lvls, columns=tests),
+        "write_t": pd.DataFrame(index=comp_lvls, columns=tests),
+        "read_t": pd.DataFrame(index=comp_lvls, columns=tests),
+    }
 
     for t in tests:
         for c in comp_lvls:
-            tail = f"{P['lx'][0]}_{P['lx'][1]}_{P['lx'][2]}_comp{c}"
+            tail = f"{lx[0]}_{lx[1]}_{lx[2]}_comp{c}"
 
             try:
                 with h5py.File(data_dir / f"{t}_{tail}.h5.write_stat.h5", "r") as f:
-                    write_datarate[t][c] = f["/median_MBsec"][()]
+                    data["write_dr"][t][c] = f["/median_MBsec"][()]
+                    data["write_t"][t][c] = np.median(f["/t_ms"][:])
             except FileNotFoundError:
                 print(f"ERROR: {t} comp_lvl {c}: write benchmark not found")
 
             try:
                 with h5py.File(data_dir / f"{t}_{tail}.h5.read_stat.h5", "r") as f:
-                    read_datarate[t][c] = f["/median_MBsec"][()]
+                    data["read_dr"][t][c] = f["/median_MBsec"][()]
+                    data["read_t"][t][c] = np.median(f["/t_ms"][:])
             except FileNotFoundError:
                 print(f"ERROR: {t} comp_lvl {c}: read benchmark not found")
 
-    return write_datarate, read_datarate
+    return data
 
 
 if __name__ == "__main__":
@@ -99,14 +106,30 @@ if __name__ == "__main__":
     P = cli()
     ttxt = title_meta(P["lx"], P["bin_dir"])
 
-    write_datarate, read_datarate = get_bench(P["lx"], P["data_dir"])
+    data = get_bench(P["lx"], P["data_dir"])
 
-    fig, ax = plot_datarate(write_datarate)
+    tail = f"{P['lx'][0]}_{P['lx'][1]}_{P['lx'][2]}"
+
+    fig, ax = plot_datarate(data["write_dr"])
     ax.set_title(f"WRITE: Slab Benchmark: {ttxt}")
-    plotfn = P["data_dir"] / f"write_datarate_{P['lx'][0]}_{P['lx'][1]}_{P['lx'][2]}.png"
+    plotfn = P["data_dir"] / f"write_datarate_{tail}.png"
+    print("making", plotfn)
     fig.savefig(plotfn, dpi=150)
 
-    fig, ax = plot_datarate(read_datarate)
+    fig, ax = plot_datarate(data["read_dr"])
     ax.set_title(f"READ: Slab Benchmark: {ttxt}")
-    plotfn = P["data_dir"] / f"read_datarate_{P['lx'][0]}_{P['lx'][1]}_{P['lx'][2]}.png"
+    plotfn = P["data_dir"] / f"read_datarate_{tail}.png"
+    print("making", plotfn)
+    fig.savefig(plotfn, dpi=150)
+
+    fig, ax = plot_time(data["write_t"])
+    ax.set_title(f"WRITE: Slab Benchmark: {ttxt}")
+    plotfn = P["data_dir"] / f"write_times_{tail}.png"
+    print("making", plotfn)
+    fig.savefig(plotfn, dpi=150)
+
+    fig, ax = plot_time(data["read_t"])
+    ax.set_title(f"READ: Slab Benchmark: {ttxt}")
+    plotfn = P["data_dir"] / f"read_times_{tail}.png"
+    print("making", plotfn)
     fig.savefig(plotfn, dpi=150)
