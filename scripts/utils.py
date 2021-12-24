3c5b2627d7b45d780b093f0e21a7f9cc22c73aa8
Michael Hirsch <scivision@users.noreply.github.com>
2021-12-24 15:03:09 -0500

    make more realistic test

--- scripts/utils.py
+++ scripts/utils.py
@@ -15,7 +15,7 @@ def cli() -> dict[str, T.Any]:
         default=R / "../build/src/tests",
     )
     p.add_argument("-o", "--datadir", help="data directory", default=R / "data")
-    p.add_argument("-n", help="total size of slab", type=int, nargs=3, default=[8192, 32, 64])
+    p.add_argument("-lx", help="total size of slab", type=int, nargs=3, default=[8192, 32, 64])
     p.add_argument("-Nrun", help="number of test runs", type=int, default=5)
     p.add_argument("-np", help="number of MPI processes", type=int)
     p.add_argument("-k", "--keep", help="keep output files", action="store_true")
@@ -41,13 +41,13 @@ def cli() -> dict[str, T.Any]:
     data_dir = Path(P.datadir).resolve()
     data_dir.mkdir(parents=True, exist_ok=True)
 
-    print(f"Slab size: {P.n}")
+    print(f"Slab size: {P.lx}")
 
     params = {
         "bin_dir": bin_dir,
         "data_dir": data_dir,
         "Nrun": P.Nrun,
-        "lx": P.n,
+        "lx": P.lx,
         "np": P.np,
         "keep": P.keep,
         "tests": P.tests,
