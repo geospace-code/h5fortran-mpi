406610b7763fe24a69d9af4ce135b075ec264cf4
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-03 15:39:54 -0500

    lint

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -23,3 +23,8 @@ endif()
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "default ROOT" FORCE)
 endif()
+
+# --- auto-ignore build directory
+if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
+  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
+endif()
