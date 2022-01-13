83c1a06055625dd9e0bb6933e242a846b7ec79c6
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-13 11:48:24 -0500

    add coverage test and ABI check

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,17 +1,36 @@
-if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
+# check C and Fortran compiler ABI compatibility
 
-add_compile_options(
-"$<$<COMPILE_LANGUAGE:Fortran>:-mtune=native;-Wall;-fimplicit-none>"
-"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-Wextra;-fcheck=all;-Werror=array-bounds>"
-"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>"
-)
-# -Wno-maybe-uninitialized
-elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+if(NOT abi_ok)
+  message(CHECK_START "checking that C and Fortran compilers can link")
+  try_compile(abi_ok ${CMAKE_CURRENT_BINARY_DIR}/abi_check ${CMAKE_CURRENT_LIST_DIR}/abi_check abi_check)
+  if(abi_ok)
+    message(CHECK_PASS "OK")
+  else()
+    message(FATAL_ERROR "ABI-incompatible: C compiler ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION} and Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
+  endif()
+endif()
 
+# --- compiler options
+# we left off "-std=f2018" type flags as they tend to issue false warnings
+
+if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
 add_compile_options(
 $<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>
 "$<$<COMPILE_LANGUAGE:Fortran>:-warn;-heap-arrays>"
 "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-traceback;-check;-debug>"
 )
+elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+add_compile_options(
+"$<$<COMPILE_LANGUAGE:Fortran>:-mtune=native;-Wall;-fimplicit-none>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-Wextra;-fcheck=all;-Werror=array-bounds>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace;-Wno-maybe-uninitialized>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:RelWithDebInfo>>:-Wno-maybe-uninitialized>"
+)
+endif()
 
+# --- code coverage
+if(ENABLE_COVERAGE)
+include(CodeCoverage)
+append_coverage_compiler_flags()
+set(COVERAGE_EXCLUDES ${PROJECT_SOURCE_DIR}/src/tests)
 endif()
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,27 +1,27 @@
-option(ENABLE_BENCHMARKS "write / read benchmarks")
-
 option(autobuild "autobuild libraries if not available" true)
-
 option(hdf5_parallel "enable parallel HDF5" true)
 
-option(dev "Development mode")
+option(ENABLE_BENCHMARKS "write / read benchmarks")
+
+option(ENABLE_COVERAGE "Code coverage tests")
+
+option(dev "developer mode")
 
 option(ourFindMPI "Use our FindMPI")
 
-option(zlib_legacy "legacy ZLIB 1.x")
-
+option(zlib_legacy "use old ZLIB 1.x")
 
 set(CMAKE_TLS_VERIFY true)
 
 if(dev)
-
 else()
   set_directory_properties(PROPERTIES EP_UPDATE_DISCONNECTED true)
 endif()
 
-
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-  set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "default ROOT" FORCE)
+  # will not take effect without FORCE
+  # CMAKE_BINARY_DIR in case it's used from FetchContent
+  set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
 endif()
 
 # --- auto-ignore build directory
