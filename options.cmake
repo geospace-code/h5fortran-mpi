3704445977156174412ce1262e168a1b5926c0f9
scivision <scivision@users.noreply.github.com>
2023-12-11 11:36:46 -0500

    add find_hdf5 option (default true)

--- options.cmake
+++ options.cmake
@@ -1,6 +1,6 @@
-include(GNUInstallDirs)
-
 message(STATUS "${PROJECT_NAME} ${PROJECT_VERSION}  CMake ${CMAKE_VERSION}")
+
+option(find_hdf5 "search for HDF5 and Zlib" true)
 
 option(benchmark "Run benchmarks")
 option(coverage "Code coverage tests")
@@ -16,6 +16,7 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND ${PROJECT_NAME}_IS_TOP_LEVEL)
   set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/local" CACHE PATH "install prefix" FORCE)
 endif()
 
+set_property(DIRECTORY PROPERTY EP_UPDATE_DISCONNECTED true)
 
 if(BUILD_SHARED_LIBS AND MSVC)
   message(WARNING "Intel oneAPI has trouble with shared libs in general on Windows, try
